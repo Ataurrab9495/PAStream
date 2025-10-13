2294a8ccf241cc5922d2dcc58876cc443ed413ff
Md Ataurrab <47085844+Ataurrab9495@users.noreply.github.com>
2025-10-14 02:17:25 +0530

    Updated useLogout.js

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,9 +1,11 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout } from "../lib/api";
+import { useNavigate } from "react-router";
 
 
 const useLogout = () => {
     const queryClient = useQueryClient();
+    const navigate = useNavigate();
 
     const {
         mutate: logoutMutation,
@@ -11,7 +13,10 @@ const useLogout = () => {
         error,
     } = useMutation({
         mutationFn: logout,
-        onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["authUser"] });
+            navigate("/login");
+        },
     });
 
     return { logoutMutation, isPending, error };
