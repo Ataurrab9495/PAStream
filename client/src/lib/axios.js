61f1a834fd276d58df0a364f0aa1604d74814eb1
Ataurrab Ansari <mohammedataurrab@gmail.com>
2025-10-17 01:01:18 +0530

    backend change

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/v1/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/v1/api';
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
